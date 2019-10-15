6f78bb9d075e622c0ab3ef327332d3aadfbb3cc0
Maciej Żychliński <mcj.zychlinski@gmail.com>
2019-10-15 13:37:05 +0200

    change like to ilike

--- app/api/volumes.py
+++ app/api/volumes.py
@@ -12,23 +12,23 @@ def get_all_volumes():
 
 @api.route('/volumes/title/<title>')
 def get_volumes_title(title):
-    volumes = Volumes.query.filter(Volumes.title.like('%{}%'.format(title)))
+    volumes = Volumes.query.filter(Volumes.title.ilike('%{}%'.format(title)))
     return jsonify({'volumes': [volume.to_json() for volume in volumes]})
 
 
 @api.route('/volumes/authors/<authors>')
 def get_volumes_authors(authors):
-    volumes = Volumes.query.filter(Volumes.authors.like('%{}%'.format(authors)))
+    volumes = Volumes.query.filter(Volumes.authors.ilike('%{}%'.format(authors)))
     return jsonify({'volumes': [volume.to_json() for volume in volumes]})
 
 
 @api.route('/volumes/language/<language>')
 def get_volumes_language(language):
-    volumes = Volumes.query.filter(Volumes.language.like('%{}%'.format(language)))
+    volumes = Volumes.query.filter(Volumes.language.ilike('%{}%'.format(language)))
     return jsonify({'volumes': [volume.to_json() for volume in volumes]})
 
 
 @api.route('/volumes/publishedDate/<publishedDate>')
 def get_volumes_publishedDate(publishedDate):
-    volumes = Volumes.query.filter(Volumes.publishedDate.like('%{}%'.format(publishedDate)))
+    volumes = Volumes.query.filter(Volumes.publishedDate.ilike('%{}%'.format(publishedDate)))
     return jsonify({'volumes': [volume.to_json() for volume in volumes]})
